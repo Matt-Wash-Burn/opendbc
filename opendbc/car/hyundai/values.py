f0e38be46ceb2aa4dbd5c7b840a86856665b137a
Matt Washburn <mattwashburn@microsoft.com>
2026-05-10 16:06:07 -0400

    Cleanup comment
    # Standard format: part number after a "X.XX " version stamp (e.g. "1.07 99211-S8100").
    # LX3 fallback: some new CAN FD camera FWs (e.g. b"LX31.001.001.002591000HKP_LX325_50919099211P9030")
    # have no spaces and append the part number to the end of the string.

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -712,9 +712,6 @@ HYUNDAI_VERSION_RESPONSE = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER + 0x4
 PLATFORM_CODE_FW_PATTERN = re.compile(b'((?<=' + HYUNDAI_VERSION_REQUEST_LONG[1:] +
                                       b')[A-Z]{2}[A-Za-z0-9]{0,2})')
 DATE_FW_PATTERN = re.compile(b'(?<=[ -])([0-9]{6}$)')
-# Standard format: part number after a "X.XX " version stamp (e.g. "1.07 99211-S8100").
-# LX3 fallback: some new CAN FD camera FWs (e.g. b"LX31.001.001.002591000HKP_LX325_50919099211P9030")
-# have no spaces and append the part number to the end of the string.
 PART_NUMBER_FW_PATTERN = re.compile(b'(?:(?<=[0-9][.,][0-9]{2} )([0-9]{5}[-/]?[A-Z][A-Z0-9]{3}[0-9])'
                                     b'|([0-9]{5}[-/]?[A-Z][A-Z0-9]{3}[0-9])$)')
 
